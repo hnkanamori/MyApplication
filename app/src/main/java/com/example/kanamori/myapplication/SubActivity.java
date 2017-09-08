69d9c680096d839337fc327126c208b2d9699d7e
金森敦史 <kanamori@kanamoriatsushinoMacBook-Pro.local>
2017-09-08 13:56:22 +0900

    遷移画面の追加

--- app/src/main/java/com/example/kanamori/myapplication/SubActivity.java
+++ app/src/main/java/com/example/kanamori/myapplication/SubActivity.java
@@ -1,7 +1,9 @@
 package com.example.kanamori.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        Intent in = this.getIntent();
+        String text = in.getStringExtra("sendText");
+        TextView textView = (TextView)this.findViewById(R.id.textViewSub);
+        textView.setText(text);
     }
+
 }
